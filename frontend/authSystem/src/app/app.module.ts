73d18a890e479a86404e4a0feadd81317c7b5a48
nmargenov <nikolay_margenov@yahoo.com>
2023-06-28 17:12:05 +0300

    setup angular

---
+++ frontend/authSystem/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './main/home/home.component';
+import { LoginComponent } from './main/auth/login/login.component';
+import { RegisterComponent } from './main/auth/register/register.component';
+import { LogoutComponent } from './main/auth/logout/logout.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    LogoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
